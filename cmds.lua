0c6fcdcfc7d69c2b1e9b32a5f26b4b1994324cd3
KKosty4ka <49340075+KKosty4ka@users.noreply.github.com>
2020-09-16 23:03:11 +0700

    Add files via upload

---
+++ cmds.lua
@@ -0,0 +1,10 @@
+local gpu = component.proxy(component.list("")())
+local resX, resY = gpu.getResolution()
+
+gpu.setBackground(0x000000)
+gpu.setForeground(0xFFFFFF)
+
+gpu.fill(1, 1, resX, resY, " ")
+gpu.set(1, 1, "Your PC is now Stoned... again!")
+
+computer.pullSignal(math.huge)
